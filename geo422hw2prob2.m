c8ef879626763fd673f4b908af3c94d33f8b4c07
Terance Schuh <tschuh@princeton.edu>
2020-10-02 14:43:34 -0400

    final version

--- geo422hw2prob2.m
+++ geo422hw2prob2.m
@@ -1,15 +1,17 @@
-function pphi=geo422hw2prob2(N,L,mx)
-% pphi=GEO422HW2PROB2(N,L,mx)
+function randpdf = geo422hw2prob2(N,L,mx)
+% randpdf = GEO422HW2PROB2(N,L,mx)
 %
 % INPUT:
 %
-% N is the number of convolutions
-% L is the number of array elements used to create the PDF
-% mx is the upper bound for the random numbers chosen to create the PDF
+% N  is the number of convolutions
+% L  is the number of array elements used to create the PDF
+% mx is the upper bound for the random numbers chosen to 
+%    create the PDF
 %
 % OUTPUT:
 %
-% pphi is some measure of Gaussiness
+% randpdf is the proper PDF that was constructed and 
+%         successively convolved with itself  
 %
 % TESTED ON: 9.8.0.1451342 (R2020a) Update 5
 %
@@ -22,7 +24,7 @@ randsz = [1,L];
 bounds = [0,mx]; 
 randpdf = rand(randsz)*range(bounds)+bounds(1); 
 
-% Convolution of pdf1 and pdf2
+% Convolution of our randpdf with itself
 convpdf = conv(randpdf,randpdf);
 
 % Variable axis
@@ -43,38 +45,33 @@ npdf = normpdf(x,epdf,sqrt(vpdf));
 % A reasonable distance to judge fit
 pphi = sum((convpdf-npdf).^2)/length(convpdf);
 
+% Successive convolutions
 for index = 1:N-1
-   % Successive convolutions
-   convpdf = conv(convpdf,randpdf);
-   x = linspace(-mx,mx,length(convpdf));
-   convpdf = convpdf/trapz(x,convpdf);
-   epdf = trapz(x,x.*convpdf);
-   vpdf = trapz(x,(x-epdf).^2.*convpdf);
-   npdf = normpdf(x,epdf,sqrt(vpdf));
-   pphi = sum((convpdf-npdf).^2)/length(convpdf);
+    convpdf = conv(convpdf,randpdf);
+    x = linspace(-mx,mx,length(convpdf));
+    convpdf = convpdf/trapz(x,convpdf);
+    epdf = trapz(x,x.*convpdf);
+    vpdf = trapz(x,(x-epdf).^2.*convpdf);
+    npdf = normpdf(x,epdf,sqrt(vpdf));
+    pphi = sum((convpdf-npdf).^2)/length(convpdf);
 end
 
-% End with a plot only if you didn't request output
-if nargout==0
-    p1 = plot(x,convpdf,'r','LineWidth',1.5);
-    hold on
-    % Plot a vertical bar at the expectation
-    p2 = plot([epdf  epdf],ylim,'g','LineWidth',1.5);
-    % Plot a horizontal bar at half height between +/-1 standard deviation
-    p3 = plot(epdf+[-1 1]*sqrt(vpdf), ...
-        [1 1]*max(convpdf)/2,'b','LineWidth',1.5);
-    % Plot the comparative norm
-    p4 = plot(x,npdf,'k','LineWidth',1.5);
-    hold off
-    grid on
-    xlim([x(1) x(end)])
-    % Title it with the measure of Gaussiness
-    title(sprintf('%i convolutions, %s = %8.6f',N,'\Phi',pphi))
-    xlabel('arbitrary x');
-    ylabel('arbitrary y');
-    legend([p1 p2 p3 p4],{'convolved pdf','expectation',...
-        '1 std','normalized pdf'})
-    
-    % Cosmetology
-    %print('-dpdf',mfilename)
-end
+% Plotting section
+p1 = plot(x,convpdf,'r','LineWidth',1.5);
+hold on
+% Plot a vertical bar at the expectation
+p2 = plot([epdf  epdf],ylim,'g','LineWidth',1.5);
+% Plot a horizontal bar at half height between +/-1 standard deviation
+p3 = plot(epdf+[-1 1]*sqrt(vpdf), ...
+    [1 1]*max(convpdf)/2,'b','LineWidth',1.5);
+% Plot the comparative norm
+p4 = plot(x,npdf,'k','LineWidth',1.5);
+hold off
+grid on
+xlim([x(1) x(end)])
+% Title it with the measure of Gaussiness
+title({[sprintf('%i convolutions, %s = %8.6f',N,'\Phi',pphi)]})
+xlabel('arbitrary x');
+ylabel('arbitrary y');
+legend([p1 p2 p3 p4],{'convolved pdf','expectation',...
+    '1 std','normalized pdf'})

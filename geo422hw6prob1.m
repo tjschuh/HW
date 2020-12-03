9025d09bafd8c5909e0c2b930a8285394d7348cc
Terance Schuh <tschuh@princeton.edu>
2020-12-02 20:54:52 -0500

    working version, ask Frederik how complex function needs to be

--- geo422hw6prob1.m
+++ geo422hw6prob1.m
@@ -1,29 +1,86 @@
-w = pi/4; %angular frequency
-Nrate = pi/w; %Nyquist rate
-deltax = 1/Nrate; %max sampling rate, computed from Nyquist rate
+function Nrate = geo422hw6prob1(w,amp,xshift,yshift)
+% Nrate = GEO422HW5(w,amp,xshift,yshift)
+%
+% INPUT:
+%
+% w       angular frequency
+% amp     amplitude of signal
+% xshift  right/left shift of signal
+% yshift  up/down shift of signal
+%
+% OUTPUT:
+%
+% Nrate   Nyquist rate computed from twice
+%         the highest frequency in signal
+%
+% TESTED ON: 9.8.0.1451342 (R2020a) Update 5
+%
+% Written by tschuh@princeton.edu, 12/1/2020
+
+%defining variables
+defval('w',3*pi/8);
+defval('amp',7);
+defval('xshift',0);
+defval('yshift',1);
+
+%only use this section if I decide to use more complex function
+%w1 = 13*pi/16;
+%w2 = 15*pi/16;
+%if w1 > w2
+%    w = w1;
+%elseif w1 < w2
+%    w = w2;
+%else
+%    w = w1;
+%end
+
+%other variables needed
+xmax = 19; %upper bound of samples
+hpsrate = 0.1; %high precision sampling rate
+
+%Nyquist rate and min sampling rate computed from Nyquist rate
+Nrate = w/pi;
+deltax = 1/(2*Nrate);
 
 %plot the true function using high precision
-n = 0:0.001:99;
-x = 7*sin(w*n);
-plot(n,x)
-hold on
+x = 0:hpsrate:xmax;
+y = amp*sin(w*x - xshift) + yshift;
+%y = sin(w1*x).*cos(w2*x);
+subplot(4,2,1)
+plot(x,y)
+title('True Plot of Function')
+subplot(4,2,2)
+periodogram(y)
 
 %plot using the Nyquist rate
-srate = deltax + .4; %Nrate goes as 1/srate
-n = 0:srate:99;
-x = 7*sin(w*n);
-plot(n,x)
-hold on
+srate = deltax; %Nrate goes as 1/srate
+x = 0:srate:xmax;
+y = amp*sin(w*x - xshift) + yshift;
+%y = sin(w1*x).*cos(w2*x);
+subplot(4,2,3)
+plot(x,y)
+title('Sampling Rate = Nyquist Rate')
+subplot(4,2,4)
+periodogram(y)
 
-%plot using a larger Nyquist rate
-srate = deltax - .24; %Nrate goes as 1/srate
-n = 0:srate:99;
-x = 7*sin(w*n);
-plot(n,x)
-hold on
+%plot using a larger Nyquist rate (should still work)
+srate = deltax - (0.5*deltax); %Nrate goes as 1/srate
+x = 0:srate:xmax;
+y = amp*sin(w*x - xshift) + yshift;
+%y = sin(w1*x).*cos(w2*x);
+subplot(4,2,5)
+plot(x,y)
+title('Sampling Rate > Nyquist Rate')
+subplot(4,2,6)
+periodogram(y)
 
-%plot using a smaller Nyquist rate
-%srate = deltax + 1; %Nrate goes as 1/srate
-%n = 0:srate:19;
-%x = 7*sin(w*n);
-%plot(n,x)
+%plot using a smaller Nyquist rate (should blow-up)
+srate = deltax + (0.5*deltax); %Nrate goes as 1/srate
+x = 0:srate:xmax;
+y = amp*sin(w*x - xshift) + yshift;
+%y = sin(w1*x).*cos(w2*x);
+subplot(4,2,7)
+plot(x,y)
+title('Sampling Rate < Nyquist Rate')
+subplot(4,2,8)
+periodogram(y)

23ee6ef8b1453036e394d83123d2fa94a8654240
Terance Schuh <tschuh@princeton.edu>
2021-03-09 00:22:47 -0500

    final version

--- geo419hw3prob1.m
+++ geo419hw3prob1.m
@@ -14,18 +14,19 @@ MV = x1*M1 + x2*M2;
 MR = 1./((x1/M1) + (x2/M2));
 
 plot(x1,MV,'b')
-hold on
-plot(x1,MR,'r')
-
 xlabel('x_1')
 ylabel('M_V, M_R')
-legend('M_V','M_R')
-
-
 ax1=gca;
+hold on
+plot(x1,MR,'r')
+legend('M_V','M_R')
 ax2 = axes('Position', get(ax1, 'Position'),'Color', 'none');
 set(ax2, 'XAxisLocation', 'top','YAxisLocation','Right');
 % set the same Limits and Ticks on ax2 as on ax1;
 set(ax2, 'XLim', get(ax1, 'XLim'),'YLim', get(ax1, 'YLim'));
 set(ax2, 'XTick', get(ax1, 'XTick'), 'YTick', get(ax1, 'YTick'));
-OppTickLabels = {'1' '0.9' '0.8' '0.7' '0.6' '0.5' '0.4' '0.3' '0.2' '0.1' '0'};
+OppTickLabelsX = {'1' '0.9' '0.8' '0.7' '0.6' '0.5' '0.4' '0.3' '0.2' '0.1' '0'};
+OppTickLabelsY = {'10' '20' '30' '40' '50' '60' '70' '80' '90' '100'};
+% Set the x-tick and y-tick  labels for the second axes
+set(ax2, 'XTickLabel', OppTickLabelsX,'YTickLabel',OppTickLabelsY);
+xlabel('x_2')

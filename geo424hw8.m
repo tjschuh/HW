dad4a921f3bffb38bb91454c1b88c93c506294ed
Terance Schuh <tschuh@princeton.edu>
2021-04-07 23:20:52 -0400

    final version

--- geo424hw8.m
+++ geo424hw8.m
@@ -1,9 +1,19 @@
-function [M,iteration] = geo424hw8(x,y,z,t)
-% [M,iteration] = GEO422HW5(x,y,z,t)
-
+function geo424hw8(x,y,z,t,numit)
+% GEO424HW8(x,y,z,t,numit)
+%
+% INPUT:
+%
+% x      initial earthquake location x-coordinate guess in km (can be +/-)
+% y      initial earthquake location y-coordinate guess in km (can be +/-)
+% z      initial earthquake location z-coordinate guess in km (can be +/-)
+% t      initial earthquake location t-coordinate guess in s (can be +/-)
+% numit  desired number of iterations, usually 6 is enough
+%
+% TESTED ON: 9.8.0.1451342 (R2020a) Update 5
+%
 % Written by tschuh@princeton.edu, 4/6/2021
 
-% initial earthquake guess [x y z t]
+% initial earthquake guess [x y z t], space in km, time in s
 M = [x; y; z; t];
 
 % load in station data
@@ -15,30 +25,26 @@ T = stationdata(:,4);
 % medium velocity [km/s]
 v = 6;
 
-% number of iterations
-numit = 10;
-iteration = 1;
+iteration = 1
 
 %calculate everything once to get a baseline result
-t = forward(S,M,v); %predicted time (fancy g in notes)
+t = forward(S,M,v); %predicted time
 K = sensitivity(S,M,v,t);
-[dM,deltad] = invert(K,T,t);
-phi(iteration) = misfit(deltad); %store each phi into an array
-M = M + dM;
+dM = invert(K,T,t);
+M = M + dM
 
 %calculate a new location M numit-1 more times
 for i = 1:numit-1
-    iteration = iteration + 1;
+    iteration = iteration + 1
     t = forward(S,M,v);
     K = sensitivity(S,M,v,t);
-    [dM,deltad] = invert(K,T,t);
-    phi(iteration) = misfit(deltad);
-    M = M + dM;
+    dM = invert(K,T,t);
+    M = M + dM
 end
 
 % plotting
 figure
-subplot(2,3,1)
+subplot(2,2,1)
 scatter(S(:,1),S(:,2),'^','filled')
 hold on
 scatter(M(1),M(2),'*','LineWidth',1.25)
@@ -47,7 +53,7 @@ xlabel('X [km]')
 ylabel('Y [km]')
 grid on
 
-subplot(2,3,2)
+subplot(2,2,2)
 scatter(S(:,1),S(:,3),'^','filled')
 hold on
 scatter(M(1),M(3),'*','LineWidth',1.25)
@@ -56,7 +62,7 @@ xlabel('X [km]')
 ylabel('Z [km]')
 grid on
 
-subplot(2,3,3)
+subplot(2,2,3)
 scatter(S(:,2),S(:,3),'^','filled')
 hold on
 scatter(M(2),M(3),'*','LineWidth',1.25)
@@ -65,7 +71,7 @@ xlabel('Y [km]')
 ylabel('Z [km]')
 grid on
 
-subplot(2,3,4)
+subplot(2,2,4)
 scatter3(S(:,1),S(:,2),S(:,3),'^','filled')
 hold on
 scatter3(M(1),M(2),M(3),'*','LineWidth',1.25)
@@ -73,24 +79,6 @@ title('XYZ 3D Positioning Of Stations')
 xlabel('X [km]')
 ylabel('Y [km]')
 zlabel('Z [km]')
-grid on
-
-subplot(2,3,5)
-semilogy(phi,'-o','MarkerSize',8)
-title('Misfit Evolving Over Iterations')
-xlabel('Iteration')
-ylabel('Misfit')
-xlim([0.5 size(phi,2)+0.5])
-ylim([0.1*min(phi) 10*max(phi)])
-yticks([.1 1 10 100 1000 10000 100000 1000000])
-grid on
-
-subplot(2,3,6)
-stem(transpose(deltad))
-title({'Current Residuals Between Observed','And Predicted Travel Times'})
-xlabel('Station Number')
-ylabel('Residual [s]')
-xlim([0 size(S,1)+1])
 grid on
 
 %forward model to calculate predicted travel time t
@@ -109,10 +97,6 @@ for j = 1:size(S,1)
 end
 
 %compute delta m using K and delta d
-function [dM,deltad] = invert(K,T,t)
+function dM = invert(K,T,t)
 deltad = T - t; %residuals (observed - predicted)
 dM = inv(transpose(K)*K)*transpose(K)*deltad;
-
-%phi tells us how "good" our current prediction is
-function phi = misfit(deltad)
-phi = (sum(deltad))^2; %sums of squares of observed - predicted
